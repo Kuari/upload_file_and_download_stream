9b8cebfa4f3c0eaf18bfaf0f3e204d38480e6f7e
kuari <kuari@justmylife.cc>
2019-05-12 12:32:08 +0800

    remove vuerouter

--- src/main.js
+++ src/main.js
@@ -3,27 +3,11 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 import App from './App.vue'
-import VueRouter from 'vue-router'
-
-import Uploads from './components/uploads.vue'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
-Vue.use(VueRouter)
-
-
-const routes = [{
-    path: '/',
-    component: Uploads,
-    title: 'index'
-}]
-
-const router = new VueRouter({
-    routes
-})
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
